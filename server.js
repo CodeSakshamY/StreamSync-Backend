c26ac7711e23b03430bb88855c98b51e62455060
CodeSakshamY <166237273+CodeSakshamY@users.noreply.github.com>
2025-08-31 23:02:19 +0530

    Delete server.js

--- server.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  process.env.sorojftjbkyngvanjsjl.supabase.co,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNvcm9qZnRqYmt5bmd2YW5qc2psIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjY1MTE5NCwiZXhwIjoyMDcyMjI3MTk0fQ.ZGUxiB1ca3xf89pv1zmbsDYu9wwsJJCvH2iNQHeGaqY
-);
